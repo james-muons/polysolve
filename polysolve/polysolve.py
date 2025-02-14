45fb1f2890b8e2ff7933863ba37b9274a11a92c5
james-muons <james.lord@stfc.ac.uk>
2025-02-14 15:06:11 +0000

    Documentation 2

--- polysolve/polysolve.py
+++ polysolve/polysolve.py
@@ -1,5 +1,6 @@
 """ The code to solve maths """
-
+#from __future__ import annotations
+from typing import Tuple
 import numpy as np
 
 CBRT_UNITY_IM = np.sqrt(3)/2 * 1j
@@ -9,7 +10,55 @@ def quadratic(
         a: float,
         b: float,
         c: float,
-        ) -> tuple[float,float]:
+        ) -> Tuple[float,float]:
+    """
+     Solves the roots of a quadratic equation.
+
+     Uses the quadratic formula. Result must be real.
+
+     Parameters
+     ----------
+     a
+        :math:`x^2` coefficient.
+     b
+        :math:`x` coefficient.
+     c
+        Constant value.
+
+    Returns
+    -------
+    tuple[float, float]
+        Positive and negative roots of quadratic.
+
+    Raises
+    ------
+    ValueError
+        Discriminant < 0 implying imaginary root.
+
+    Notes
+    -----
+    Equation of the form:
+
+    .. math::
+
+        ax^{2} + bx + c
+
+    Examples
+    --------
+    >>> quadratic(1., 0., 0.)
+    (0.0, -0.0)
+    >>> quadratic(3., 0., -1.)
+    (0.5773502691896257, -0.5773502691896257)
+
+    See Also
+    --------
+    numpy.polyval : Evaluate polynomial at point.
+
+    References
+    ----------
+    .. [1] O. McNoleg, "The integration of GIS, remote sensing,
+           expert systems ...
+    """
     det = b**2 - (4*a*c)
 
     return ((-b + np.sqrt(det)) / (2*a),
@@ -20,7 +69,57 @@ def cubic(
         b: float,
         c: float,
         d: float,
-        ) -> tuple[float,float,float]:
+        ) -> Tuple[float,float,float]:
+    """
+     Solves the roots of a quadratic equation.
+
+     Uses the cubic formula. Result must be real.
+
+     Parameters
+     ----------
+     a
+        :math:`x^3` coefficient.
+     b
+        :math:`x^2` coefficient.
+     c
+        :math:`x` coefficient.
+     d
+        Constant value.
+
+    Returns
+    -------
+    tuple[float, float, float]
+        Positive, middle and negative roots of cubic.
+
+    Raises
+    ------
+    ValueError
+        Discriminant < 0 implying imaginary root.
+
+    Notes
+    -----
+    Equation of the form:
+
+    .. math::
+
+        ax^{2} + bx + c
+
+    Examples
+    --------
+    >>> quadratic(1., 0., 0.)
+    (0.0, -0.0)
+    >>> quadratic(3., 0., -1.)
+    (0.5773502691896257, -0.5773502691896257)
+
+    See Also
+    --------
+    numpy.polyval : Evaluate polynomial at point.
+
+    References
+    ----------
+    .. [1] O. McNoleg, "The integration of GIS, remote sensing,
+           expert systems ...
+    """
     q = (3*a*c - b**2) / (9*a**2)
     r = (9*a*b*c - 27*a**2*d - 2*b**3) / (54*a**3)
 
